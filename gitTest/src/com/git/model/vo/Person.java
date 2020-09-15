5b183b4744440c06adcca80f3ed2f3d18a75d307
user <user@LAPTOP-EC8717LQ>
2020-09-15 11:16:07 +0900

    04_person add properties 주소 키 추가

--- gitTest/src/com/git/model/vo/Person.java
+++ gitTest/src/com/git/model/vo/Person.java
@@ -1,11 +1,47 @@
 package com.git.model.vo;
 
 public class Person {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//회원정보저장
-
+       private String name;
+       private int age;
+       private String address;
+       private double height;
+       public Person() {
+		// TODO Auto-generated constructor stub
+     	   
 	}
 
-}
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
+       
+	}
+
+
